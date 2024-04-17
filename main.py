8809c1d7747c275ded784f042b258b9be0432ee7
UditSrinivasan <uditsrini2001@gmail.com>
2024-04-17 18:31:40 +0530

    model trainer added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from CancerClassification import logger
 from CancerClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CancerClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from CancerClassification.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -25,3 +25,13 @@ except Exception as e:
         logger.exception(e)
         raise e
     
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
